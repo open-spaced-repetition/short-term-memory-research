2a0ff535a79964b1b76245ea6393d362c03d3835
Jarrett Ye <jarrett.ye@outlook.com>
2024-04-06 19:34:59 +0800

    update dataset

--- first_forgetting_curve.py
+++ first_forgetting_curve.py
@@ -2,75 +2,138 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../fsrs-optimizer/src/fsrs_optimizer/"))
-from fsrs_optimizer import remove_outliers, power_forgetting_curve
+from fsrs_optimizer import (
+    remove_outliers,
+    remove_non_continuous_rows,
+    power_forgetting_curve,
+)
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from pathlib import Path
 from scipy.optimize import minimize
+from itertools import accumulate
 
 plt.style.use("ggplot")
 
-for path in Path("../fsrs-dataset/short-term/").glob("revlog_history-*.tsv"):
-    df = pd.read_csv(path, sep="\t")
-    try:
-        df[df["i"] == 2] = (
-            df[df["i"] == 2]
-            .groupby(by=["r_history", "t_history"], as_index=False, group_keys=False)
-            .apply(remove_outliers)
-        )
-    except:
-        continue
-    
-    ratings = set()
-    for r_history in df[df["i"] == 2]["r_history"].value_counts().head(5).index:
-        tmp = (
-            df[df["r_history"] == r_history]
-            .groupby("delta_t")
-            .agg(
-                {
-                    "y": ["mean", "count"],
-                }
+
+def cum_concat(x):
+    return list(accumulate(x))
+
+
+def create_time_series(df):
+    df = df[df["rating"].isin([1, 2, 3, 4])].copy()
+    df["i"] = df.groupby("card_id").cumcount() + 1
+    t_history_list = df.groupby("card_id", group_keys=False)["delta_t"].apply(
+        lambda x: cum_concat([[i] for i in x])
+    )
+    r_history_list = df.groupby("card_id", group_keys=False)["rating"].apply(
+        lambda x: cum_concat([[i] for i in x])
+    )
+    df["r_history"] = [
+        ",".join(map(str, item[:-1])) for sublist in r_history_list for item in sublist
+    ]
+    df["t_history"] = [
+        ",".join(map(str, item[:-1])) for sublist in t_history_list for item in sublist
+    ]
+    last_rating = []
+    for t_sublist, r_sublist in zip(t_history_list, r_history_list):
+        for t_history, r_history in zip(t_sublist, r_sublist):
+            flag = True
+            for t, r in zip(reversed(t_history[:-1]), reversed(r_history[:-1])):
+                if t > 0:
+                    last_rating.append(r)
+                    flag = False
+                    break
+            if flag:
+                last_rating.append(r_history[0])
+    df["last_rating"] = last_rating
+    df["y"] = df["rating"].map(lambda x: {1: 0, 2: 1, 3: 1, 4: 1}[x])
+    df = df[df["delta_t"] != 0].copy()
+    df["i"] = df.groupby("card_id").cumcount() + 1
+    df["first_rating"] = df["r_history"].map(lambda x: x[0] if len(x) > 0 else "")
+    filtered_dataset = (
+        df[df["i"] == 2]
+        .groupby(by=["first_rating"], as_index=False, group_keys=False)
+        .apply(remove_outliers)
+    )
+    if filtered_dataset.empty:
+        return pd.DataFrame()
+    df[df["i"] == 2] = filtered_dataset
+    df.dropna(inplace=True)
+    df = df.groupby("card_id", as_index=False, group_keys=False).apply(
+        remove_non_continuous_rows
+    )
+    return df[df["delta_t"] > 0].sort_values(by=["review_th"])
+
+
+if __name__ == "__main__":
+    sorted_files = sorted(
+        Path("../fsrs-benchmark/dataset").glob("*.csv"), key=lambda x: int(x.stem)
+    )[:32]
+    for path in sorted_files:
+        df = create_time_series(pd.read_csv(path))
+        try:
+            df[df["i"] == 2] = (
+                df[df["i"] == 2]
+                .groupby(
+                    by=["r_history", "t_history"], as_index=False, group_keys=False
+                )
+                .apply(remove_outliers)
             )
-        )
-        delta_t = tmp.index
-        y_mean = tmp["y"]["mean"]
-        y_count = tmp["y"]["count"]
-        count_percent = np.array([x / sum(y_count) for x in y_count])
-        weight = np.sqrt(y_count)
+        except:
+            continue
 
-        def loss(stability):
-            y_pred = power_forgetting_curve(delta_t, stability)
-            logloss = sum(
-                -(y_mean * np.log(y_pred) + (1 - y_mean) * np.log(1 - y_pred)) * weight
+        ratings = set()
+        for r_history in df[df["i"] == 2]["r_history"].value_counts().head(5).index:
+            tmp = (
+                df[df["r_history"] == r_history]
+                .groupby("delta_t")
+                .agg(
+                    {
+                        "y": ["mean", "count"],
+                    }
+                )
             )
-            return logloss
+            delta_t = tmp.index
+            y_mean = tmp["y"]["mean"]
+            y_count = tmp["y"]["count"]
+            count_percent = np.array([x / sum(y_count) for x in y_count])
+            weight = np.sqrt(y_count)
 
-        res = minimize(
-            loss,
-            x0=1,
-            bounds=((0.01, 100),),
-        )
-        params = res.x
-        stability = params[0]
+            def loss(stability):
+                y_pred = power_forgetting_curve(delta_t, stability)
+                logloss = sum(
+                    -(y_mean * np.log(y_pred) + (1 - y_mean) * np.log(1 - y_pred))
+                    * weight
+                )
+                return logloss
 
-        plt.figure(r_history[0])
-        ratings.add(r_history[0])
-        plt.plot(
-            np.linspace(0, 10),
-            power_forgetting_curve(np.linspace(0, 10), stability),
-            label=f"r_history={r_history}|stability={stability:.2f}|count={sum(y_count)}",
-        )
-        plt.scatter(delta_t, y_mean, count_percent * 1000, alpha=0.5)
-        plt.xlim(0, 10)
-        plt.ylim(0, 1)
-        plt.legend()
-        plt.xlabel("delta_t")
-        plt.ylabel("retention")
-        plt.title(f"user {path.stem.split('-')[1]}")
+            res = minimize(
+                loss,
+                x0=1,
+                bounds=((0.01, 100),),
+            )
+            params = res.x
+            stability = params[0]
 
-    for rating in ratings:
-        plt.figure(str(rating))
-        plt.savefig(f"./first_forgetting_curve/{path.stem.split('-')[1]}-{rating}.png")
-        plt.cla()
+            plt.figure(r_history[0])
+            ratings.add(r_history[0])
+            plt.plot(
+                np.linspace(0, 10),
+                power_forgetting_curve(np.linspace(0, 10), stability),
+                label=f"r_history={r_history}|stability={stability:.2f}|count={sum(y_count)}",
+            )
+            plt.scatter(delta_t, y_mean, count_percent * 1000, alpha=0.5)
+            plt.xlim(0, 10)
+            plt.ylim(0, 1)
+            plt.legend()
+            plt.xlabel("delta_t")
+            plt.ylabel("retention")
+            plt.title(f"user {path.stem}")
+
+        for rating in ratings:
+            plt.figure(str(rating))
+            plt.savefig(f"./first_forgetting_curve/{path.stem}-{rating}.png")
+            plt.cla()
